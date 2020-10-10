e38f61ab519a7a6535e296558fbc1d1ddd0636dc
Salem Eid <salem.eid@gmail.com>
2020-10-10 13:09:07 +0300

    added shop page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
-import { Switch, Route, Link } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component.jsx'
+import { Switch, Route } from 'react-router-dom';
 
-const HatPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/crwn-clothing' component={HomePage}/>
-        <Route path='/crwn-clothing/hats' component={HatPage}/>
+        <Route path='/crwn-clothing/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
