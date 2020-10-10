c29819cb38e0202a19bd113afb002a4d8020f1f6
Salem Eid <salem.eid@gmail.com>
2020-10-10 08:18:48 +0300

    updated routing phase test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import { Switch, Route, Link } from 'react-router-dom';
 
+const HatPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/crwn-clothing' component={HomePage}/>
+        <Route path='/crwn-clothing/hats' component={HatPage}/>
+      </Switch>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 import './homepage.styles.scss'
 
 import '../../components/directory-menu/directory-menu.component'
